460fd4f1ed45bba0028f74ddc00c5928d86d3d44
Trung Kiên <25023280@vnu.edu.vn>
2026-05-10 17:55:57 +0700

    Update AuthenticationException.java

--- src/AuctionSystem/exception/AuthenticationException.java
+++ src/AuctionSystem/exception/AuthenticationException.java
@@ -1,6 +1,10 @@
 package AuctionSystem.exception;
 
 public class AuthenticationException extends RuntimeException {
+    public AuthenticationException() {
+        super("Xác thực thất bại. Vui lòng đăng nhập lại.");
+    }
+
     public AuthenticationException(String message) {
         super(message);
     }
