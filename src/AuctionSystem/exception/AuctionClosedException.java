86dfb9dfcc2c59c9caeedbc3f6909db6dde18519
Trung Kiên <25023280@vnu.edu.vn>
2026-05-10 17:55:40 +0700

    exception

--- src/AuctionSystem/exception/AuctionClosedException.java
+++ src/AuctionSystem/exception/AuctionClosedException.java
@@ -1,6 +1,10 @@
 package AuctionSystem.exception;
 
 public class AuctionClosedException extends RuntimeException {
+    public AuctionClosedException() {
+        super("Phiên đấu giá đã kết thúc, không thể đặt giá mới.");
+    }
+
     public AuctionClosedException(String message) {
         super(message);
     }
